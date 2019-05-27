dfaca717c88aa8c544f2dbb0cd000ebaef7917a9
Sachin Jat <sachin@aurodata.com>
2019-05-27 21:54:58 +0530

    open chrome in backgraound

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'sqlite3', '1.3.5'
+gem 'watir'
+gem 'rails', '3.2.13'
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headless::Application.config.secret_token = '82444d05a9cd651d6aec9900fea966bb425b4954c924db2af7487bcbd1d16b3735c7c8ac419d33552aa0e34b20056c1181bdd2284e326c6b5dfb6f152463ab47'
